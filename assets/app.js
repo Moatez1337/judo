364a2c79fabcb8b4a6eff99efa1387efc56483a4
moatez1337 <mo3tazz99@gmail.com>
2024-10-01 00:55:24 +0200

    added encore and bootstrap

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,8 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// Import Bootstrap's JavaScript
+import 'bootstrap';
+
+// Import Bootstrap's CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
